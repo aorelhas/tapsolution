3ee60b628aaaa980697ed6265eb474cdbca77a7c
Carlos Orelhas <c.orelhas@gmail.com>
2020-06-08 21:34:13 +0100

    User Schema

---
+++ models/User.js
@@ -0,0 +1,33 @@
+const mongoose = require('mongoose');
+
+const UserSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    about: {
+      type: String,
+      trim: true,
+    },
+    role: {
+      type: Number,
+      default: 0,
+    },
+    historu: {
+      type: Array,
+      default: [],
+    },
+  },
+  { timestamps: true }
+);
+
+module.exports = User = mongoose.model('user', UserSchema);
--- server.js
+++ server.js
@@ -12,7 +12,7 @@ app.use(express.json({ extennded: false }));
 app.get('/', (req, res) => res.send('API Running..'));
 
 // Routes Definition
-// app.use('/api/users', require('./routes/api/users'));
+app.use('/users', require('./routes/user'));
 
 const PORT = process.env.PORT || 5000;
 

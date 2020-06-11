7e5980be27da6f5f9ce3dfcf13b3519d76f558ae
Carlos Orelhas <c.orelhas@gmail.com>
2020-06-11 23:24:52 +0100

    Create Product

--- routes/api/product.js
+++ routes/api/product.js
@@ -2,6 +2,9 @@ const express = require('express');
 const router = express.Router();
 // const auth = require('../../middleware/auth');
 // const admin = require('../../middleware/admin');
+const formidable = require('formidable');
+const _ = require('lodash');
+const fs = require('fs');
 const config = require('config');
 const { check, validationResult } = require('express-validator');
 
@@ -10,8 +13,27 @@ const Product = require('../../models/Product');
 // @route   POST api/product/create
 // @desc    Create Product
 // @access  Private
-router.get('/create', (req, res) => {
-  res.send('test');
+router.post('/create/:user_id', async (req, res) => {
+  let form = new formidable.IncomingForm();
+  form.keepExtensions = true;
+  form.parse(req, (err, fields, files) => {
+    if (err) {
+      return res.status(400).json({ msg: err.array() });
+    }
+
+    let product = new Product(fields);
+
+    if (files.photo) {
+      product.photo.data = fs.readFileSync(files.photo.path);
+      product.photo.contentType = files.photo.type;
+    }
+
+    product.save((err, result) => {
+      if (err) throw err;
+
+      res.json(result);
+    });
+  });
 });
 
 module.exports = router;
